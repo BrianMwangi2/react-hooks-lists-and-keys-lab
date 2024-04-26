46f6c3610eba02cb7341bd7f8f16ff4a78d8018f
Brian <brian.mwangi2@student.moringaschool.com>
2024-04-26 08:13:09 +0300

    passes

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,43 @@
+
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
-function ProjectList({ projects }) {
-  console.log(projects);
+function ProjectList() {
+  const projects = [
+    {
+      id: 1,
+      name: "Reciplease",
+      about: "A recipe tracking app",
+      technologies: ["Rails", "Bootstrap CSS"],
+    },
+    {
+      id: 2,
+      name: "Kibbles N Bitz",
+      about: "Tinder for dogs",
+      technologies: ["React", "Redux"],
+    },
+    {
+      id: 3,
+      name: "Alienwares",
+      about: "Etsy for aliens",
+      technologies: ["React", "Redux", "Rails"],
+    },
+      
+  ];
+
   return (
-    <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+    <div>
+      {projects.map((project, index) => (
+        <ProjectItem
+          key={index} // added key for ProjectItem
+          name={project.name}
+          about={project.about}
+          technologies={project.technologies}
+        />
+      ))}
     </div>
   );
 }
 
 export default ProjectList;
+
